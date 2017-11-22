757e57f640191bdf7d9aea4251c241f3aa9ebb6a
Sudan <sudansth@gmail.com>
2017-11-22 11:49:24 +1100

    Adding test for BV_MOVE

--- src/test/java/life/genny/rules/RuleTest.java
+++ src/test/java/life/genny/rules/RuleTest.java
@@ -4,8 +4,11 @@ import java.util.HashMap;
 
 import java.util.Map;
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.junit.Before;
 import org.junit.Test;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -30,9 +33,22 @@ public class RuleTest {
 	 }
 	 
 
-	// @Test
+	@Test
      public void bucket_view_drag_drop_test() {
+		System.out.println("Hello");
 		 final Map<String, String> keyValue = new HashMap<String, String>();
+//		 final Keycloak kc = KeycloakBuilder.builder()
+//		          .serverUrl("http://10.1.120.89:8180/auth")
+//		          .realm("genny")
+//		          .username("user1")
+//		          .password("password1")
+//		          .clientId("curl")
+//		          .resteasyClient(new ResteasyClientBuilder().connectionPoolSize(10).build())
+//		          .build();
+//		 String token = kc.tokenManager().getAccessToken().getToken();
+//		 String token1 = kc.tokenManager().getAccessTokenString();
+//		 System.out.println("The token is: "+token);
+//		 System.out.println("The token is: "+token1);
 		 
 		 QEventLinkChangeMessage evtMsg = new 
 			      QEventLinkChangeMessage("GRP_QUOTES","GRP_COMPLETED", "BEG_0000002", "LNK_CORE", null);
