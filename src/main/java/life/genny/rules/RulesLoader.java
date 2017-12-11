10b5ea7376392a768bc259ef63f83e31a0ebdbf3
Adam Crow <acrow@Adams-MacBook-Pro.local>
2017-12-11 16:53:51 +1100

    Added initial rulesloader

---
+++ src/main/java/life/genny/rules/RulesLoader.java
@@ -0,0 +1,15 @@
+package life.genny.rules;
+
+import io.vertx.rxjava.core.Future;
+import io.vertx.rxjava.core.Vertx;
+import life.genny.cluster.ClusterConfig;
+
+public class RulesLoader {
+//	  public static Future<Void> joinCluster(final Vertx vertx) {
+//		    final Future<Void> fut = Future.future();
+//		    vertx.rxClusteredVertx(ClusterConfig.configCluster()).subscribe(registerAllChannels,
+//		        clusterError);
+//		    fut.complete();
+//		    return fut;
+//		  }
+}
