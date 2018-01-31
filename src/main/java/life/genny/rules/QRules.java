868edfb1f0254bb3e63fc6b31bee8d10f3a99c12
rpgayatri <rpgayatri@gmail.com>
2018-01-31 14:56:55 +1100

    Rules for make and release payment

--- src/main/java/life/genny/rules/QRules.java
+++ src/main/java/life/genny/rules/QRules.java
@@ -1353,7 +1353,7 @@ public class QRules {
 			msg.setToken(getToken());
 			publish("cmds", RulesUtils.toJsonObject(msg));
 
-			QCmdViewMessage cmdFormView = new QCmdViewMessage("CMD_VIEW", questionCode);
+			QCmdViewMessage cmdFormView = new QCmdViewMessage("FORM_VIEW", questionCode);
 			publishCmd(cmdFormView);
 
 		} catch (IOException e) {
