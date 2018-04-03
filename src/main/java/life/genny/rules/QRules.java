9f1b59bbdc0896e29ecd940ce8791a73b1351cc4
Anish Maharjan <anish.maharjan12341@gmail.com>
2018-04-03 11:41:03 +1000

    renamed subscribeUserToBaseEntityCode to subscribeUserToBaseEntity

--- src/main/java/life/genny/rules/QRules.java
+++ src/main/java/life/genny/rules/QRules.java
@@ -2798,7 +2798,7 @@ public class QRules {
 		return results;
 	}
 
-	public void subscribeUserToBaseEntityCode(String userCode, String beCode) {
+	public void subscribeUserToBaseEntity(String userCode, String beCode) {
 		VertxUtils.subscribe(realm(), beCode, userCode);
 	}
 
