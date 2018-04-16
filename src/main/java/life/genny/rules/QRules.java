be97e55416211c7ae3ea79b6b943d50e5d8c4518
rpgayatri <rpgayatri@gmail.com>
2018-04-16 18:20:22 +1000

    generic attachments

--- src/main/java/life/genny/rules/QRules.java
+++ src/main/java/life/genny/rules/QRules.java
@@ -4513,25 +4513,6 @@ public class QRules {
 
 	}
 	
-	//attachments
-		public void sendMessage(String begCode, String[] recipientArray, HashMap<String, String> contextMap,
-					String templateCode, String messageType, List<QBaseMSGAttachment> attachmentList) {
-
-				if (recipientArray != null && recipientArray.length > 0) {
-
-					/* Adding project code to context */
-					String projectCode = "PRJ_" + getAsString("realm").toUpperCase();
-					contextMap.put("PROJECT", projectCode);
-
-					JsonObject message = MessageUtils.prepareMessageTemplateWithAttachments(templateCode, messageType, contextMap,
-							recipientArray, attachmentList, getToken());
-					publish("messages", message);
-				} else {
-					log.error("Recipient array is null and so message cant be sent");
-				}
-
-		}
-
 	/*
 	 * Publish Search BE results
 	 */
@@ -4566,6 +4547,25 @@ public class QRules {
 		return false;
 
 	}
+	
+	//attachments
+		public void sendMessage(String begCode, String[] recipientArray, HashMap<String, String> contextMap,
+					String templateCode, String messageType, List<QBaseMSGAttachment> attachmentList) {
+
+				if (recipientArray != null && recipientArray.length > 0) {
+
+					/* Adding project code to context */
+					String projectCode = "PRJ_" + getAsString("realm").toUpperCase();
+					contextMap.put("PROJECT", projectCode);
+
+					JsonObject message = MessageUtils.prepareMessageTemplateWithAttachments(templateCode, messageType, contextMap,
+							recipientArray, attachmentList, getToken());
+					publish("messages", message);
+				} else {
+					log.error("Recipient array is null and so message cant be sent");
+				}
+
+		}
 
 	/*
 	 * Give oldChat for the given sender and receiver
