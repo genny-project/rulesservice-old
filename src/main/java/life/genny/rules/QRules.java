bbe9583941085778457213e1cf556a9aba9103d4
Sudan <sudansth@gmail.com>
2018-04-19 18:31:41 +1000

    Rebase conflict resolution

--- src/main/java/life/genny/rules/QRules.java
+++ src/main/java/life/genny/rules/QRules.java
@@ -3919,6 +3919,26 @@ public class QRules {
 	/*
 	 *  Publish all the messages that belongs to the given chat
 	 */
+	public void sendChatMessages(final String chatCode) {
+		publishBaseEntitysByParentAndLinkCodeWithAttributes(chatCode, "LNK_MESSAGES", 0, 100, true);
+
+		SearchEntity report = new SearchEntity("SBE_CHATMSGS","Chat Messages")
+		  	     .addColumn("PRI_MESSAGE","Message")
+		  	     .addColumn("PRI_CREATOR","Creater ID")
+
+		  	     .setSourceCode(chatCode)
+
+		  	     .addSort("PRI_CREATED","Created",SearchEntity.Sort.DESC)
+		  	     .addFilter("PRI_CODE",SearchEntity.StringFilter.LIKE,"MSG_%")
+
+		  	     .setPageStart(0)
+		  	     .setPageSize(100);
+
+	}
+
+	/*
+	 *  Publish all the messages that belongs to the given chat
+	 */
 	public void sendChatMessages(final String chatCode, final int pageStart, final int pageSize) {
 		//publishBaseEntitysByParentAndLinkCodeWithAttributes(chatCode, "LNK_MESSAGES", 0, 100, true);
 
