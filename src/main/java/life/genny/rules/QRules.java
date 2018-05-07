8026a434194fb7ddeec16bc3b2d35110f30cb126
devjs <loris1634@gmail.com>
2018-05-07 14:09:12 +1000

    internmatch registration workflow

--- src/main/java/life/genny/rules/QRules.java
+++ src/main/java/life/genny/rules/QRules.java
@@ -309,6 +309,16 @@ public class QRules {
 	public Boolean is(final String key) {
 		return decodedTokenMap.containsKey(key);
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Object> getAsList(final String key) {
+		return (List<Object>) get(key);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Object[] getAsArray(final String key) {
+		return (Object[]) get(key);
+	}
 
 	@SuppressWarnings("unchecked")
 	public List<Object> getAsList(final String key) {
