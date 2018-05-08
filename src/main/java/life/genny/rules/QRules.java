3dd8b3b10220d8d783fe3ea98a1c6fea8eb58870
rpgayatri <rpgayatri@gmail.com>
2018-05-08 17:40:19 +1000

    rename driver-pdf name

--- src/main/java/life/genny/rules/QRules.java
+++ src/main/java/life/genny/rules/QRules.java
@@ -5595,8 +5595,7 @@ public class QRules {
 			/* invoice attachment for driver */
 			if (driverInvoiceLayoutUrl != null) {
 				driverAttachmentList = new ArrayList<>();
-				/*QBaseMSGAttachment driverInvoiceAttachment = new QBaseMSGAttachment(AttachmentType.INLINE,
-						"application/pdf", driverInvoiceLayoutUrl, true, "INVOICE_PDF");*/
+
 				QBaseMSGAttachment driverInvoiceAttachment = new QBaseMSGAttachment(AttachmentType.INLINE, "application/pdf", driverInvoiceLayoutUrl, true, "RECEIPT_PDF");
 
 				driverAttachmentList.add(driverInvoiceAttachment);
