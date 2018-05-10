2c86b57d8b1a645387854a7e50525245934b7a69
Adam Crow <acrow@Adams-Compooter.local>
2018-05-10 22:14:09 +1000

    upgraded to 1.0.28

--- src/main/java/life/genny/rules/QRules.java
+++ src/main/java/life/genny/rules/QRules.java
@@ -5131,10 +5131,12 @@ public class QRules {
 				String keycloakurl = realmJson.getString("auth-server-url").substring(0,
 						realmJson.getString("auth-server-url").length() - ("/auth".length()));
 
+					
 				try {
-					AccessTokenResponse accessToken = KeycloakUtils.getAccessToken(keycloakurl, realm(), realm(),
+					String token = KeycloakUtils.getToken(keycloakurl, realm(), realm(),
 							secret, "service", password);
-					String token = accessToken.getToken();
+//					String token = accessToken.getToken();
+//					String token = QwandaUtils.apiGet(qwandaServiceUrl+"/utils/token/"+keycloakurl+"/{realm}/"+secret+"/"+key+"/"+initVector+"/service/"+encryptedPassword,"DUMMY");
 
 					Map<String, Object> serviceDecodedTokenMap = KeycloakUtils.getJsonMap(token);
 
@@ -5507,4 +5509,5 @@ public class QRules {
 
 	}
 
+	
 }
