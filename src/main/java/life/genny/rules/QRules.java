1f82d6d1dd7e937f75a042b6ec84e12ebf6f76b6
rpgayatri <rpgayatri@gmail.com>
2018-05-24 10:46:43 +1000

    redirect to home if item creation fails

--- src/main/java/life/genny/rules/QRules.java
+++ src/main/java/life/genny/rules/QRules.java
@@ -8035,23 +8035,25 @@ public class QRules {
 				/* Convert dollars into cents */
 				BigDecimal finalFee = itemPrice.multiply(new BigDecimal(100));
 				Money moneyInCents = Money.of(finalFee, ownerAmountWithoutFee.getCurrency());
-				System.out.println("money in cents ::"+moneyInCents);
+				println("money in cents ::"+moneyInCents);
 		
 				try {			
 					/* get fee */
 					String paymentFeeId = createPaymentFee(offerBe, paymentsToken);
 					System.out.println("payment fee Id ::"+paymentFeeId);
 					String[] feeArr = { paymentFeeId };
-					
+										
 					/* bundling all the info into Item object */
 					QPaymentsItem item = new QPaymentsItem(paymentsItemName, begDescription, PaymentTransactionType.escrow,
 							moneyInCents.getNumber().doubleValue(), ownerAmountWithoutFee.getCurrency(), feeArr, buyer, seller);
-					
+										
 					/* Hitting payments item creation API */
 					String itemCreationResponse = PaymentEndpoint.createPaymentItem(JsonUtils.toJson(item), paymentsToken);
 					
-					QPaymentsAssemblyItemResponse itemResponsePojo = JsonUtils.fromJson(itemCreationResponse, QPaymentsAssemblyItemResponse.class);
-					itemId = itemResponsePojo.getId();
+					if(itemCreationResponse != null) {
+						QPaymentsAssemblyItemResponse itemResponsePojo = JsonUtils.fromJson(itemCreationResponse, QPaymentsAssemblyItemResponse.class);
+						itemId = itemResponsePojo.getId();
+					}
 					
 				} catch (PaymentException e) {
 					String getFormattedErrorMessage = getPaymentsErrorResponseMessage(e.getMessage());
@@ -8059,6 +8061,9 @@ public class QRules {
 				}
 
 			} catch (IllegalArgumentException e) {
+				
+				/* Send back to home page */
+				redirectToHomePage();
 				
 				String jobId = begBe.getValue("PRI_JOB_ID", null);
 				BaseEntity userBe = getUser();
