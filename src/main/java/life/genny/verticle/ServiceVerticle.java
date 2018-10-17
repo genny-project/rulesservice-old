4c8a4eb08c5af650e9615be2c3451492aad5d321
Adam Crow <acrow@Adams-Compooter.local>
2018-10-17 23:35:42 +1100

    Set up code to assist common rules

--- src/main/java/life/genny/verticle/ServiceVerticle.java
+++ src/main/java/life/genny/verticle/ServiceVerticle.java
@@ -1,16 +1,27 @@
 package life.genny.verticle;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+
+import javax.naming.NamingException;
 
 import org.apache.logging.log4j.Logger;
 
+import com.google.common.io.Files;
+
 import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.core.Future;
+import io.vertx.rxjava.core.Vertx;
+import io.vertx.rxjava.core.eventbus.EventBus;
 import life.genny.channel.Routers;
 import life.genny.channels.EBCHandlers;
 import life.genny.cluster.Cluster;
 import life.genny.cluster.CurrentVtxCtx;
+import life.genny.eventbus.EventBusInterface;
+import life.genny.eventbus.EventBusVertx;
+import life.genny.qwanda.message.QEventMessage;
 import life.genny.qwandautils.GennySettings;
 import life.genny.rules.RulesLoader;
 import life.genny.security.SecureResources;
@@ -29,14 +40,16 @@ public class ServiceVerticle extends AbstractVerticle {
     final Future<Void> startFuture = Future.future();
     Cluster.joinCluster().compose(res -> {
       final Future<Void> fut = Future.future();
-       RulesLoader.loadInitialRules(GennySettings.rulesDir).compose(p -> {
+      EventBusInterface eventBus = new EventBusVertx();
+      
+       loadInitialRules(GennySettings.rulesDir).compose(p -> {
   //      Routers.routers(vertx);
         
         // Load in realm data
         final Future<Void> rfut = Future.future();
         SecureResources.setKeycloakJsonMap().compose(r -> {
      	    final Future<Void> startupfut = Future.future();
-    	     RulesLoader.triggerStartupRules(GennySettings.rulesDir).compose(q -> {
+    	     triggerStartupRules(GennySettings.rulesDir, eventBus).compose(q -> {
     	        startupfut.complete();
     	        
     	    }, startupfut);
@@ -47,7 +60,7 @@ public class ServiceVerticle extends AbstractVerticle {
           rfut.complete();
         }, rfut);
         
-        EBCHandlers.registerHandlers(CurrentVtxCtx.getCurrentCtx().getClusterVtx().eventBus());
+        EBCHandlers.registerHandlers(eventBus);
         
 
         fut.complete();
@@ -58,4 +71,41 @@ public class ServiceVerticle extends AbstractVerticle {
    
 
   }
+  
+	/**
+	 * @param vertx
+	 * @return
+	 */
+	public static Future<Void> loadInitialRules(final String rulesDir) {
+		
+		final Future<Void> fut = Future.future();
+		Vertx.currentContext().owner().executeBlocking(exec -> {
+			RulesLoader.loadRules(rulesDir);
+
+			fut.complete();
+		}, failed -> {
+		});
+
+		return fut;
+	}
+	
+	/**
+	 * @param vertx
+	 * @return
+	 */
+	public static Future<Void> triggerStartupRules(final String rulesDir, EventBusInterface eventBus) {
+		log.info("Triggering Startup Rules for all realms");
+		final Future<Void> fut = Future.future();
+		Vertx.currentContext().owner().executeBlocking(exec -> {// Force Genny first
+			log.info("---- Realm:genny Startup Rules ----------");
+
+
+			RulesLoader.initMsgs("Event:INIT_STARTUP", new QEventMessage("EVT_MSG", "INIT_STARTUP"), eventBus);
+			fut.complete();
+		}, failed -> {
+		});
+
+		return fut;
+	}
+
 }
